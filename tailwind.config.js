22ac79fd6fc03b6a4870ddfa01de2a66afcc6a2c
Kristin C <7070136+krievley@users.noreply.github.com>
2022-07-23 14:25:53 -0400

    Add colors to Tailwind config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,22 @@ module.exports = {
     ],
 
     theme: {
+        colors: {
+            brand: '#bf201d',
+            brandHover: '#991B1B',
+            light: '#f4f2f1',
+            lightAccent: '#d67621',
+            lightAccentHover: '#b5631b',
+            dark: '#34283e',
+            darkAccent: '#b58b7f',
+            darkAccentHover: '#d6a496',
+            primary: '#bf201d',
+            info: '#33283e',
+            success: '#6f8441',
+            warning: '#ec7409',
+            danger: '#f44336',
+            disabled: '#d2d2d2',
+        },
         extend: {
             fontFamily: {
                 'display': ['Oswald', 'sans-serif'],
